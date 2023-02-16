0c8397297990409fc0b61ea60831083ed49e150c
grecemam <isip_m.s.maksimenkova@mpt.ru>
2023-02-16 13:23:55 +0300

    spasibo

---
+++ каленжарь_еды))/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace каленжарь_еды__
+{
+    public partial class MainWindow : Window
+    {
+        public Ejednevnik ejednevnik = new Ejednevnik();
+        public MainWindow()
+        {
+            InitializeComponent();
+            datesel.SelectedDate = ejednevnik.data;
+            UpdateListBox();
+        }
+
+        private void UpdateListBox()
+        {
+            SpisonZametok.Items.Clear();
+            foreach (int i in ejednevnik.today)
+            {
+                SpisonZametok.Items.Add(ejednevnik.notes[i].name);
+            }
+        }
+        private void DateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            nazvanie.Text = "";
+            opis.Text = "";
+            save.IsEnabled = false;
+            ejednevnik.data = (DateTime)datesel.SelectedDate;
+            ejednevnik.LoadNotes();
+            UpdateListBox();
+        }
+        private void create_Click(object sender, RoutedEventArgs e)
+        {
+            string name = nazvanie.Text;
+            string opisanie = opis.Text;
+            nazvanie.Text = "";
+            opis.Text = "";
+            DateTime date = ejednevnik.data;
+            Note note = new Note(name, opisanie, date);
+            ejednevnik.AddNote(note);
+            UpdateListBox();
+        }
+
+        private void SpisonZametok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selected = SpisonZametok.SelectedIndex;
+            if (selected == -1)
+            {
+                return;
+            }
+            int index = ejednevnik.today[selected];
+            Note note = ejednevnik.notes[index];
+            nazvanie.Text = note.name;
+            opis.Text = note.opis;
+            save.IsEnabled = true;
+            del.IsEnabled = true;
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            int selected = SpisonZametok.SelectedIndex;
+            Note note = new Note(nazvanie.Text, opis.Text, ejednevnik.data);
+            ejednevnik.notes[selected] = note;
+            save.IsEnabled = false;
+            nazvanie.Text = "";
+            opis.Text = "";
+            ejednevnik.LoadNotes();
+            UpdateListBox();
+        }
+
+        private void del_Click(object sender, RoutedEventArgs e)
+        {
+            int selected = SpisonZametok.SelectedIndex;
+            ejednevnik.notes.RemoveAt(selected);
+            del.IsEnabled = false;
+            nazvanie.Text = "";
+            opis.Text = "";
+            ejednevnik.LoadNotes();
+            UpdateListBox();
+        }
+    }
+}
---
+++ каленжарь_еды))/Note.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace каленжарь_еды__
+{
+
+    public class Note
+    {
+        public string name;
+        public string opis;
+        public DateTime data;
+        public Note(string name, string opis, DateTime data)
+        {
+            this.name = name;
+            this.opis = opis;
+            this.data = data;
+        }
+    }
+    public class Ejednevnik
+    {
+        public List<Note> notes;
+        public List<int> today = new List<int>();
+        public DateTime data;
+        public Ejednevnik()
+        {
+            this.data = DateTime.Today.Date;
+            List<Note> notes = DataWork.Deserialize<List<Note>>("spisok.json");
+            this.notes = notes;
+            LoadNotes();
+        }
+        public void AddNote(Note note)
+        {
+            this.notes.Add(note);
+            LoadNotes();
+            DataWork.Serialize(this.notes, "spisok.json");
+        }
+        public void LoadNotes()
+        {
+            this.today.Clear();
+            for (int i = 0; i < this.notes.Count; i++)
+            {
+                Note note = this.notes[i];
+                if (note.data == this.data)
+                {
+                    this.today.Add(i);
+                }
+            }
+            DataWork.Serialize(this.notes, "spisok.json");
+        }
+    }
+
+    public class DataWork
+    {
+        public static T Deserialize<T>(string path)
+        {
+            try
+            {
+                T znachenie = JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+                return znachenie;
+            }
+            catch
+            {
+                File.WriteAllText(path, "[]");
+                return Deserialize<T>(path);
+            }
+        }
+        public static void Serialize(object obj, string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(obj));
+        }
+    }
+}
